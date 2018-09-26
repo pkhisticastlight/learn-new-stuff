f821d0db2429bc8e0145d7d26453df36783a2d45
Priya Khisti <pkhisti@castlighthealth.com>
2018-09-26 12:26:30 -0700

    Initial commit with all the files

---
+++ src/app/rest-api.service.ts
@@ -0,0 +1,94 @@
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { catchError, tap, map } from 'rxjs/operators';
+
+const httpOptions = {
+  headers: new HttpHeaders({'Content-Type': 'application/json'})
+};
+// const apiUrl = "http://localhost:1337/localhost:3000/api/classroom";
+const apiUrl = "http://192.168.0.7:3000/api/classroom";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RestApiService {
+
+  constructor(private http: HttpClient) { }
+  classes = ['Java','React','Angular'];
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError('Something bad happened; please try again later.');
+  }
+
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+
+  getClassroom(): Observable<any> {
+     let observable=Observable.create(observer => {
+          setTimeout(() => {
+           let listing = this.generateMockListing();
+            observer.next(listing);
+            observer.complete();
+          }, 2000);
+
+        });
+    return observable;
+  }
+
+  getClassroomById(id: string): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.get(url, httpOptions).pipe(
+      map(this.extractData),
+      catchError(this.handleError));
+  }
+
+  postClassroom(data): Observable<any> {
+    const url = `${apiUrl}/add_with_students`;
+    return this.http.post(url, data, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  updateClassroom(id: string, data): Observable<any> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.put(url, data, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  deleteClassroom(id: string): Observable<{}> {
+    const url = `${apiUrl}/${id}`;
+    return this.http.delete(url, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  generateMockListing(): Array<any> {
+    let mockListing : Array<any>;
+    mockListing = new Array<any>();
+    for(let i = 1; i <= 5 ; i++) {
+      let listing: any = new Object;
+      listing.id = i;
+      listing.class_name = this.classes[Math.floor(Math.random()*this.classes.length)];
+      listing.createdAt = new Date(); 
+      mockListing.push(listing);
+    }
+    return mockListing;
+  }
+}
